7bce17c610c6863bed8df5d89e89aa710366ff1f
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-04 22:53:21 +0100

    changement nav

--- assets/js/allezCine.js
+++ assets/js/allezCine.js
@@ -1,6 +1,9 @@
 var animTime = 300;
 var clickPolice = false;
 
+var price = parseFloat($("#prc1").text());
+var quantity = $("#qt1").val();
+
 // ACCORDEON
   $(document).on('touchstart click', '.acc-btn', function(){
     if(!clickPolice){
@@ -20,147 +23,83 @@ var clickPolice = false;
     
   });
 
-// <button id="btn+1">+</button>
-// <button id="btn-1">-</button> : 
-// <input type="text" class="quantity" id="qt1" name="qt1" value="0">
-// <span id="tot1">0 €</span>
-
-var price = parseFloat($("#prc1").text());
-var quantity = $("#qt1").val();
-
-function sousTotal(){
-  $('#calculate').click()
+// CALCUL PRIX
+$('#calculate').click(function(){
+      alert(quantity*price);
+  });
 
 
-  alert(quantity*price);
-}
 
-sousTotal();
+// ONGLETS
+(function onglets() {
 
+    /*
+    LORSQUE l'on clique sur un onglet
+        * ON RETIRE la class active de l'onglet actif
+        * J'ajoute la class active à l'onglet actuel
 
-/* Set values + misc */
-// var promoCode;
-// var promoPrice;
-// var fadeTime = 300;
+        ON retire la class active sur le contenu actif
+        j'ajoute la class active sur le contenu correspondant à mon clic
+    */
 
-// /* Assign actions */
-// $('.quantity input').change(function() {
-//   updateQuantity(this);
-// });
+    var afficherOnglet = function (a, animations) {
+        if (animations === undefined) {
+            animations = true
+        }
+        var li = a.parentNode
+        var div = a.parentNode.parentNode.parentNode
+        var activeTab = div.querySelector('.tab-contents.active')  // contenu actif
+        var aAfficher = div.querySelector(a.getAttribute('href')) // contenu à afficher
 
-// $('.remove button').click(function() {
-//   removeItem(this);
-// });
+        if(li.classList.contains('active')){
+            return false
+        }
 
-// $(document).ready(function() {
-//   updateSumItems();
-// });
+        div.querySelector('.tabs .active').classList.remove('active')
+        li.classList.add('active')
 
-// $('.promo-code-cta').click(function() {
+        if (animations) {
+            activeTab.classList.add('fade')
+            activeTab.classList.remove('in')
+            var transitionend = function () {
+                this.classList.remove('fade')
+                this.classList.remove('active')
+                aAfficher.classList.add('active')
+                aAfficher.classList.add('fade')
+                aAfficher.offsetWidth
+                aAfficher.classList.add('in')
+                this.removeEventListener('transitionend', transitionend)
+                this.removeEventListener('webkitTransitionEnd', transitionend)
+                this.removeEventListener('oTransitionEnd', transitionend)
+                this.removeEventListener('mozTransitionEnd', transitionend)
+            }
+            activeTab.addEventListener('transitionend', transitionend)
+            activeTab.addEventListener('webkitTransitionEnd', transitionend)
+            activeTab.addEventListener('oTransitionEnd', transitionend)
+            activeTab.addEventListener('mozTransitionEnd', transitionend)
+        } else {
+            aAfficher.classList.add('active')
+            activeTab.classList.remove('active')
+        }
+        // On ajoute la class fade sur l'élément actif
+        // A la fin de l'animation
+        //     On retire la class fade et active
+        //     On ajoute la class active et fade à l'élément à afficher
+        //     ON ajoute la class in
+    }
 
-//   promoCode = $('#promo-code').val();
+    var tabs = document.querySelectorAll('.tabs a')
+    for (var i = 0; i < tabs.length; i++){
+        tabs[i].addEventListener('click', function (e) {
+            afficherOnglet(this)
+        })
+    }
+})
 
-//   if (promoCode == 'BECODE' || promoCode == 'becode') {
-//     //If promoPrice has no value, set it as 10 for the 10OFF promocode
-//     if (!promoPrice) {
-//       promoPrice = 10;
-//     } else if (promoCode) {
-//       promoPrice = promoPrice * 1;
-//     }
-//   } else if (promoCode != '') {
-//     alert("Code promo invalide");
-//     promoPrice = 0;
-//   }
-//   //If there is a promoPrice that has been set (it means there is a valid promoCode input) show promo
-//   if (promoPrice) {
-//     $('.summary-promo').removeClass('hide');
-//     $('.promo-value').text(promoPrice.toFixed(2));
-//     recalculateCart(true);
-//   }
-// });
-
-// /* Recalculate cart */
-// function recalculateCart(onlyTotal) {
-//   var subtotal = 0;
-
-//   /* Sum up row totals */
-//   $('.basket-product').each(function() {
-//     subtotal += parseFloat($(this).children('.subtotal').text());
-//   });
-
-//   /* Calculate totals */
-//   var total = subtotal;
-
-//   //If there is a valid promoCode, and subtotal < 10 subtract from total
-//   var promoPrice = parseFloat($('.promo-value').text());
-//   if (promoPrice) {
-//     if (subtotal >= 10) {
-//       total -= promoPrice;
-//     } else {
-//       alert('La commande doit être supérieure à 10 € pour que le code promo s\'applique.');
-//       $('.summary-promo').addClass('hide');
-//     }
-//   }
-
-//   /*If switch for update only total, update only total display*/
-//   if (onlyTotal) {
-//      Update total display 
-//     $('.total-value').fadeOut(fadeTime, function() {
-//       $('#basket-total').html(total.toFixed(2));
-//       $('.total-value').fadeIn(fadeTime);
-//     });
-//   } else {
-//     /* Update summary display. */
-//     $('.final-value').fadeOut(fadeTime, function() {
-//       $('#basket-subtotal').html(subtotal.toFixed(2));
-//       $('#basket-total').html(total.toFixed(2));
-//       if (total == 0) {
-//         $('.checkout-cta').fadeOut(fadeTime);
-//       } else {
-//         $('.checkout-cta').fadeIn(fadeTime);
-//       }
-//       $('.final-value').fadeIn(fadeTime);
-//     });
-//   }
-// }
-
-// /* Update quantity */
-// function updateQuantity(quantityInput) {
-//   /* Calculate line price */
-//   var productRow = $(quantityInput).parent().parent();
-//   var price = parseFloat(productRow.children('.price').text());
-//   var quantity = $(quantityInput).val();
-//   var linePrice = price * quantity;
-
-//   /* Update line price display and recalc cart totals */
-//   productRow.children('.subtotal').each(function() {
-//     $(this).fadeOut(fadeTime, function() {
-//       $(this).text(linePrice.toFixed(2));
-//       recalculateCart();
-//       $(this).fadeIn(fadeTime);
-//     });
-//   });
-
-//   productRow.find('.item-quantity').text(quantity);
-//   updateSumItems();
-// }
-
-// function updateSumItems() {
-//   var sumItems = 0;
-//   $('.quantity input').each(function() {
-//     sumItems += parseInt($(this).val());
-//   });
-//   $('.total-items').text(sumItems);
-// }
-
-// /* Remove item from cart */
-// function removeItem(removeButton) {
-//   /* Remove row from DOM and recalc cart total */
-//   var productRow = $(removeButton).parent().parent();
-//   productRow.slideUp(fadeTime, function() {
-//     productRow.remove();
-//     recalculateCart();
-//     updateSumItems();
-//   });
-// }
-
+    /*
+    JE RECUPERE le hash
+    AJOUTER LA CLASS active sur le lien href="hash"
+    RETIRER LA CLASS active sur les autres onglets
+    AFFICHER / Masquer les contenus
+    */
+  ();
