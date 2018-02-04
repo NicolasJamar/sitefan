ebb3d3eccea115dceb35be35c65a4cd66351b435
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-04 12:34:35 +0100

    initial2

--- assets/js/allezCine.js
+++ assets/js/allezCine.js
@@ -14,6 +14,27 @@ $('#show').click(function(){
 // 	} 
 // })
 
+  var animTime = 300,
+      clickPolice = false;
+  
+  $(document).on('touchstart click', '.acc-btn', function(){
+    if(!clickPolice){
+       clickPolice = true;
+      
+      var currIndex = $(this).index('.acc-btn'),
+          targetHeight = $('.acc-content-inner').eq(currIndex).outerHeight();
+   
+      $('.acc-btn h3').removeClass('selected');
+      $(this).find('h3').addClass('selected');
+      
+      $('.acc-content').stop().animate({ height: 0 }, animTime);
+      $('.acc-content').eq(currIndex).stop().animate({ height: targetHeight }, animTime);
+
+      setTimeout(function(){ clickPolice = false; }, animTime);
+    }
+    
+  });
+
 
 /* Set values + misc */
 var promoCode;
